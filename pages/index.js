c2859ba773aadfee17e4efe6be7a02420686c120
James Driscoll <jamesdriscoll@outlook.com>
2022-08-24 13:09:08 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import material, { FormControlLabel } from '@mui/material'
+import Slide from '@mui/material/Slide';
+import React, {useEffect, useState} from 'react';
 
 export default function Home() {
+
+const [Index, setIndex] = useState(0);
+
+useEffect(() => {
+  const intervalID = setTimeout(() =>  {
+      setIndex((Index) => prevIndex+1)
+  }, 3000);
+
+  return () => clearInterval(intervalID);
+}, []);
+
+const textArray = [
+  "digital",
+  "development",
+  "graphics",
+  "blog"
+]
+
+
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,40 +37,39 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          This is <a href="https://nextjs.org">tyty<span> {textArray[Index]}</span></a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          you are just getting started...
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>About &rarr;</h2>
+            <p>Discover tyty digital and what it can do for you.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Blog &rarr;</h2>
+            <p>Weekly updates into the world of tyty digital.</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Web Development &rarr;</h2>
+            <p>Explore what tyty web-development can achieve.</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Graphic Design &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Take centre-stage with beautiful intuative designs.
             </p>
           </a>
         </div>
